3ed10e6e74cb80811e64e6ca7127670ea4874106
ebuchheim <emma.buchheim@gmail.com>
2019-01-15 07:26:13 -0800

    add method to separate array with oxford commas

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+    length = array.count
+    if length == 1
+        array.join
+    elsif length == 2
+        array.join(" and ")
+    elsif length > 2
+        last_item = array.pop
+        last_item = "and #{last_item}"
+        array.join(" , ")
+        array.push(last_item)
+        array.join(", ")
+    else
+        puts "Can't do that, array too short."
+    end
 
 end
